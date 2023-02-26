51e2c28cab4d7b5e291a10bcb292c0b505da725c
luciano soares <lucianosoaresdev748@gmail.com>
2023-02-26 18:43:21 -0300

    add good Morning

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ function carregar() {
   const mensagem = document.getElementById("msg");
   const imagem = document.getElementById("imagem");
   var data = new Date();
-  var hora = 8; // var hora = data.getHours();
-  mensagem.innerHTML = `Agora são ${hora} horas`;
+  var hora = data.getHours();
   if (hora >= 0 && hora < 12) {
     //Bom Dia!
     imagem.src = "manha.jpg";
     document.body.style.background = "#AB3303";
+    mensagem.innerHTML = `Agora são ${hora} horas. Boa Dia!`;
   } else if (hora >= 12 && hora <= 18) {
     //Boa Tarde!
     imagem.src = "tarde.jpg";
     document.body.style.background = "#D9AC4C";
+    mensagem.innerHTML = `Agora são ${hora} horas. Boa Tarde!`;
   } else {
     //boa noite!
     imagem.src = "noite.jpg";
     document.body.style.background = "#8E82F5";
+    mensagem.innerHTML = `Agora são ${hora} horas. Boa Noite!`;
   }
 }
